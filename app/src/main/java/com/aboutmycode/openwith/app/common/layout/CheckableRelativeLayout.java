a03aeed69f286228f037d7456b7919fc5dec3ccd
Giorgi Dalakishvili <mcnamaragio@gmail.com>
2014-05-25 23:16:27 +0400

    Added CheckableRelativeLayout.

---
+++ app/src/main/java/com/aboutmycode/openwith/app/common/layout/CheckableRelativeLayout.java
@@ -0,0 +1,112 @@
+package com.aboutmycode.openwith.app.common.layout;
+
+/**
+ * Created by Giorgi on 5/25/2014.
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Checkable;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+
+public class CheckableRelativeLayout extends RelativeLayout implements Checkable {
+
+    /**
+     * Interface definition for a callback to be invoked when the checked state of this View is
+     * changed.
+     */
+    public static interface OnCheckedChangeListener {
+
+        /**
+         * Called when the checked state of a compound button has changed.
+         *
+         * @param checkableView The view whose state has changed.
+         * @param isChecked     The new checked state of checkableView.
+         */
+        void onCheckedChanged(View checkableView, boolean isChecked);
+    }
+
+    private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
+
+    private boolean isChecked = false;
+    ArrayList<Checkable> checkableViews = new ArrayList<Checkable>(8);
+
+    private OnCheckedChangeListener mOnCheckedChangeListener;
+
+    public CheckableRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean b) {
+        if (b != isChecked) {
+            isChecked = b;
+            refreshDrawableState();
+
+            if (mOnCheckedChangeListener != null) {
+                mOnCheckedChangeListener.onCheckedChanged(this, isChecked);
+            }
+
+            for (Checkable c : checkableViews) {
+                c.setChecked(isChecked);
+            }
+        }
+    }
+
+    public void toggle() {
+        setChecked(!isChecked);
+        for (Checkable c : checkableViews) {
+            c.toggle();
+        }
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        final int childCount = this.getChildCount();
+        for (int i = 0; i < childCount; ++i) {
+            findCheckableChildren(this.getChildAt(i));
+        }
+    }
+
+    private void findCheckableChildren(View v) {
+        if (v instanceof Checkable) {
+            this.checkableViews.add((Checkable) v);
+        }
+
+        if (v instanceof ViewGroup) {
+            final ViewGroup vg = (ViewGroup) v;
+            final int childCount = vg.getChildCount();
+            for (int i = 0; i < childCount; ++i) {
+                findCheckableChildren(vg.getChildAt(i));
+            }
+        }
+    }
+
+
+    @Override
+    public int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
+    }
+
+    /**
+     * Register a callback to be invoked when the checked state of this view changes.
+     *
+     * @param listener the callback to call on checked state change
+     */
+    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
+        mOnCheckedChangeListener = listener;
+    }
+}
