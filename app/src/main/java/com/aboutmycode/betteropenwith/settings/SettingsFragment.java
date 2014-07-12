231af66478d3d94cb50f2042d49c121bae3caace
Giorgi Dalakishvili <mcnamaragio@gmail.com>
2014-07-12 22:46:30 +0400

    Small tweaks to settings.

--- app/src/main/java/com/aboutmycode/betteropenwith/settings/SettingsFragment.java
+++ app/src/main/java/com/aboutmycode/betteropenwith/settings/SettingsFragment.java
@@ -1,18 +1,24 @@
 package com.aboutmycode.betteropenwith.settings;
 
 
-
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 
 import com.aboutmycode.betteropenwith.R;
 
+import java.util.prefs.Preferences;
+
 /**
  * A simple {@link Fragment} subclass.
- *
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     public SettingsFragment() {
     }
 
@@ -20,5 +26,36 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        toggleColumnsPreference(getPreferenceScreen().getSharedPreferences());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+        if (key.equals("layout")) {
+            toggleColumnsPreference(preferences);
+        }
+    }
+
+    private void toggleColumnsPreference(SharedPreferences preferences) {
+        Resources resources = getResources();
+        Preference gridColumnsPreference = getPreferenceScreen().findPreference("gridColumns");
+
+        if (preferences.getString("layout", resources.getString(R.string.listValue)).equals(resources.getString(R.string.listValue))) {
+            gridColumnsPreference.setEnabled(false);
+        } else {
+            gridColumnsPreference.setEnabled(true);
+        }
     }
 }
