30d80af8ccdcb70d4a8d8f242179aaf5480b62e9
Giorgi Dalakishvili <mcnamaragio@gmail.com>
2014-09-22 23:51:19 +0400

    Added language switcher.

--- app/src/main/java/com/aboutmycode/betteropenwith/common/YesNoDialogFragment.java
+++ app/src/main/java/com/aboutmycode/betteropenwith/common/YesNoDialogFragment.java
@@ -9,7 +9,12 @@ import android.os.Bundle;
 import com.aboutmycode.betteropenwith.R;
 
 public class YesNoDialogFragment extends DialogFragment {
+    private static String arg_param_message = "ARG_PARAM_MESSAGE";
+    private static String arg_param_title = "ARG_PARAM_TITLE";
+    private static String arg_param_show_negative = "ARG_PARAM_SHOW_NEGATIVE";
     private String message;
+    private String title;
+    private boolean showNegative;
 
     public YesNoDialogFragment() {
     }
@@ -19,14 +24,16 @@ public class YesNoDialogFragment extends DialogFragment {
         super.onCreate(savedInstanceState);
         Bundle arguments = getArguments();
         if (arguments != null) {
-            message = arguments.getString("ARG_PARAM_MESSAGE");
+            message = arguments.getString(arg_param_message);
+            title = arguments.getString(arg_param_title);
+            showNegative = arguments.getBoolean(arg_param_show_negative);
         }
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setTitle(getString(R.string.confirm));
+        alertDialogBuilder.setTitle(title);
         alertDialogBuilder.setMessage(message);
         alertDialogBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
@@ -34,16 +41,25 @@ public class YesNoDialogFragment extends DialogFragment {
                 ((YesNoListener) getActivity()).yesClicked();
             }
         });
-        alertDialogBuilder.setNegativeButton(android.R.string.cancel, null);
+
+        if (showNegative) {
+            alertDialogBuilder.setNegativeButton(android.R.string.cancel, null);
+        }
 
         return alertDialogBuilder.create();
     }
 
-    public static YesNoDialogFragment newInstance(String message) {
+    public static YesNoDialogFragment newInstance(String title, String message, boolean showNegative) {
         YesNoDialogFragment fragment = new YesNoDialogFragment();
         Bundle args = new Bundle();
-        args.putString("ARG_PARAM_MESSAGE", message);
+        args.putString(arg_param_message, message);
+        args.putString(arg_param_title, title);
+        args.putBoolean(arg_param_show_negative, showNegative);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public static YesNoDialogFragment newInstance(String title, String message) {
+        return newInstance(title, message, true);
     }
 }
